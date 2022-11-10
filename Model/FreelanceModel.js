d8c4c4363abedf6a280902e39cea3ba6537db09c
rubiagul280 <rubia.gulzar280@gmail.com>
2022-11-10 18:29:00 +0500

    ..

---
+++ Model/FreelanceModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const FreelanceSchema = mongoose.Schema(
+    {
+        postion: {
+            type: String,
+            required: [true, 'Please provide the postion.']
+        },
+        image: {
+            type: Image,
+            required: [true, 'Please upload the program image.']
+        },
+        address: {
+            type: Array,
+            platform: {
+                type: String,
+                required: [true, 'Please provide the platform.']
+            },
+            city: {
+                type: String,
+                required: [true, 'Please provide the city name.']
+            },
+            country: {
+                type: String,
+                required: [true, 'Please provide the country name.']
+            },
+            postalcode: {
+                type: Number,
+                required: [true, 'Please provide postal code of your city']
+            }  
+        },
+        startdate: {
+            type: Date,
+            required: [true, 'Pick the Start date first.']
+        },
+        enddate: {
+            type: Date,
+            required: [true, 'Pick the End date first.']
+        }
+
+})
+
+const free = mongoose.model('Freelance', FreelanceSchema);
+module.exports = free;
---
+++ Model/IndustryModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const IndustrySchema = mongoose.Schema(
+    {
+        post: {
+            type: String,
+            required: [true, 'Please provide the post.']
+        },
+        subject: {
+            type: String,
+        },
+        image: {
+            type: Image,
+            required: [true, 'Please upload the program image.']
+        },
+        address: {
+            type: Array,
+            company: {
+                type: String,
+                required: [true, 'Please provide the company name.']
+            },
+            city: {
+                type: String,
+                required: [true, 'Please provide the city name.']
+            },
+            country: {
+                type: String,
+                required: [true, 'Please provide the country name.']
+            },
+            postalcode: {
+                type: Number,
+                required: [true, 'Please provide postal code of your city']
+            }
+            
+        },
+        startdate: {
+            type: Date,
+            required: [true, 'Pick the Start date first.']
+        },
+        enddate: {
+            type: Date,
+            required: [true, 'Pick the End date first.']
+        }
+
+})
+
+const industry = mongoose.model('Industry', IndustrySchema);
+module.exports = industry;
